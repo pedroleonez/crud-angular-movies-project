e5242ae9b3e400a509388be1b55ffbd3ee66b129
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-04 14:38:13 -0300

    feat: add MatToolbarModule and MoviesListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { MoviesListComponent } from './movies/components/movies-list/movies-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MoviesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
