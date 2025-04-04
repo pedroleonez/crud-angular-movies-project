f0888a1993dfa083269538f916e34d7aa9b04211
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-04 14:34:42 -0300

    fix: adjust table

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 import { Movie } from '../../models/movie';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MoviesService } from '../../services/movies.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.scss'
 })
 export class MoviesListComponent {
 
-  movies: Movie[] = [];
+  movies: Observable<Movie[]>;
+  displayedColumns = ['title', 'director'];
+
+  constructor(private readonly moviesService: MoviesService) {
+    this.movies = this.moviesService.list();
+  }
+
+  // ngOnInit() {
+  // }
 
 }
