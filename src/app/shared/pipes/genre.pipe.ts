db057be4f8a3aecdc95ea0f62b55d9812defab96
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-04 23:20:26 -0300

    feat: create and impl genre pipe

---
+++ src/app/shared/pipes/genre.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'genre'
+})
+export class GenrePipe implements PipeTransform {
+
+  transform(value: string): string {
+    switch (value) {
+      case 'Action' : return 'cyclone';
+      case 'Comedy' : return 'smile';
+      case 'Drama' : return 'theater_comedy';
+      case 'Fantasy' : return 'stars';
+      case 'Horror' : return 'night_shelter';
+      case 'Romance' : return 'favorite';
+      case 'Thriller' : return 'dangerous';
+      case 'Sci-Fi' : return 'rocket_launch';
+    }
+    return 'help';
+  }
+
+}
