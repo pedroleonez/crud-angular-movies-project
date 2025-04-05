c95b9c2a4c4ca4f5fa8359ada896b8f35231a508
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-04 23:22:09 -0300

    feat: add error dialog and genre pipe

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -1,24 +1,45 @@
 import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { Movie } from '../../models/movie';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MoviesService } from '../../services/movies.service';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
+import {MatIconModule} from '@angular/material/icon';
+import { GenrePipe } from '../../../shared/pipes/genre.pipe';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [MatTableModule, MatCardModule, MatToolbarModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule, MatProgressSpinnerModule, CommonModule, MatIconModule, GenrePipe],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.scss'
 })
 export class MoviesListComponent {
 
-  movies: Observable<Movie[]>;
-  displayedColumns = ['title', 'director'];
+  movies$: Observable<Movie[]>;
+  displayedColumns = ['title', 'year', 'director', 'genre'];
 
-  constructor(private readonly moviesService: MoviesService) {
-    this.movies = this.moviesService.list();
+  constructor(
+    private readonly moviesService: MoviesService,
+    public dialog: MatDialog
+  ) {
+    this.movies$ = this.moviesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar a lista de filmes.');
+          return of([]);
+        })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   // ngOnInit() {
