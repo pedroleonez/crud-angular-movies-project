17dacc1cff5308f85a70e2fe8e0b4e8730431aa4
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 11:17:06 -0300

    feat: impl movie-form

--- src/app/movies/containers/movie-form/movie-form.component.ts
+++ src/app/movies/containers/movie-form/movie-form.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { RouterOutlet } from '@angular/router'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatSelectModule } from '@angular/material/select'
+
+import { MoviesService } from '../../services/movies.service'
 
 @Component({
   selector: 'app-movie-form',
-  imports: [],
+  imports: [RouterOutlet, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbar, MatSelectModule],
   templateUrl: './movie-form.component.html',
   styleUrl: './movie-form.component.scss'
 })
 export class MovieFormComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: MoviesService
+  ) {
+    this.form = this.formBuilder.group({
+      title: [null],
+      movieYear: [null],
+      director: [null],
+      genre: [null]
+    });
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(result => console.log(result));
+    this.form.reset();
+  }
+
+  onClear() {
+    this.form.reset();
+  }
 }
