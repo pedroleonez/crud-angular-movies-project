f984315c3fd58f85c93489db7324c9c700a98606
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 20:22:46 -0300

    feat: add edit route

--- src/app/movies/movies.routes.ts
+++ src/app/movies/movies.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router'
 
-import { MoviesComponent } from './components/movies/movies.component'
+import { MoviesComponent } from './containers/movies/movies.component'
 import { MovieFormComponent } from './containers/movie-form/movie-form.component'
 
 export const MOVIES_ROUTES: Routes = [
   {path: '', redirectTo: '', pathMatch: 'full'},
   {path: '', component: MoviesComponent},
   {path: 'new', component: MovieFormComponent},
+  {path: 'edit/:id', component: MovieFormComponent},
 ];
