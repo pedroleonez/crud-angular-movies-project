79196456315c165aa1714edb04e5e76c52e809ed
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 11:18:53 -0300

    feat: add save method

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { Movie } from '../../models/movie';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MoviesService } from '../../services/movies.service';
-import { catchError, Observable, of } from 'rxjs';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CommonModule } from '@angular/common';
-import { MatDialog } from '@angular/material/dialog';
-import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
-import {MatIconModule} from '@angular/material/icon';
-import { GenrePipe } from '../../../shared/pipes/genre.pipe';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core'
+import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { catchError, Observable, of } from 'rxjs'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { CommonModule } from '@angular/common'
+import { MatDialog } from '@angular/material/dialog'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { ActivatedRoute, Router } from '@angular/router'
+
+import { Movie } from '../../models/movie'
+import { MoviesService } from '../../services/movies.service'
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component'
+import { GenrePipe } from '../../../shared/pipes/genre.pipe'
 
 @Component({
   selector: 'app-movies-list',
@@ -26,7 +28,9 @@ export class MoviesListComponent {
 
   constructor(
     private readonly moviesService: MoviesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.movies$ = this.moviesService.list()
       .pipe(
@@ -45,5 +49,9 @@ export class MoviesListComponent {
 
   // ngOnInit() {
   // }
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
 
 }
