755e6899157d54067befbfe7524210cf146d6b77
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 11:20:24 -0300

    fix: make home path null

--- src/app/movies/movies.routes.ts
+++ src/app/movies/movies.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from "@angular/router";
-import { MoviesListComponent } from "./components/movies-list/movies-list.component";
-import { MovieFormComponent } from "./containers/movie-form/movie-form.component";
+import { Routes } from '@angular/router'
+
+import { MoviesListComponent } from './components/movies-list/movies-list.component'
+import { MovieFormComponent } from './containers/movie-form/movie-form.component'
 
 export const MOVIES_ROUTES: Routes = [
-  {path: '', redirectTo: 'movies-list', pathMatch: 'full'},
-  {path: 'movies-list', component: MoviesListComponent},
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {path: '', component: MoviesListComponent},
   {path: 'new', component: MovieFormComponent},
 ];
