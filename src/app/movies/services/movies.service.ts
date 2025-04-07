6bf232bd41ea88109256721e8350136b52ea1bd9
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-07 01:09:48 -0300

    feat: add post and put methods on save method

--- src/app/movies/services/movies.service.ts
+++ src/app/movies/services/movies.service.ts
@@ -23,14 +23,28 @@ export class MoviesService {
   }
 
   save(record: Partial<Movie>) {
+    if (record.id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  loadById(id: string) {
+    return this.httpClient.get<Movie>(`${this.API}/get/${id}`)
+    .pipe(
+      first()
+    );
+  }
+
+  private create(record: Partial<Movie>) {
     return this.httpClient.post<Movie>(`${this.API}/add`, record)
     .pipe(
       first()
     );
   }
 
-  loadById(id: string) {
-    return this.httpClient.get<Movie>(`${this.API}/get/${id}`)
+  private update(record: Partial<Movie>) {
+    return this.httpClient.put<Movie>(`${this.API}/update/${record.id}`, record)
     .pipe(
       first()
     );
