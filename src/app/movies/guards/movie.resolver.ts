f0aac11732aef435439def2433ad799ed0a96f3f
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 21:14:43 -0300

    feat: impl movie resolver

--- src/app/movies/guards/movie.resolver.ts
+++ src/app/movies/guards/movie.resolver.ts
@@ -1,5 +1,22 @@
 import { ResolveFn } from '@angular/router';
+import { MoviesService } from '../services/movies.service';
+import { inject } from '@angular/core';
+import { Movie } from '../models/movie';
+import { Observable, of } from 'rxjs';
 
-export const movieResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
+export const movieResolver: ResolveFn<Observable<Movie>> = (
+  route,
+  state,
+  service: MoviesService = inject(MoviesService)
+) => {
+  if (route.params?.['id']) {
+    return service.loadById(route.params['id']);
+  }
+  return of({
+    id: '',
+    title: '',
+    movieYear: null as unknown as number,
+    director: '',
+    genre: '',
+  });
 };
