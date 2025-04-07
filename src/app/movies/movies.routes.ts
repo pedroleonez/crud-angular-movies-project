7213fb19b42599fb789252cc2c0abc4376cb7d4e
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-06 21:15:13 -0300

    feat: add movieResolver to routes

--- src/app/movies/movies.routes.ts
+++ src/app/movies/movies.routes.ts
@@ -1,11 +1,20 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
 
-import { MoviesComponent } from './containers/movies/movies.component'
-import { MovieFormComponent } from './containers/movie-form/movie-form.component'
+import { MoviesComponent } from './containers/movies/movies.component';
+import { MovieFormComponent } from './containers/movie-form/movie-form.component';
+import { movieResolver } from './guards/movie.resolver';
 
 export const MOVIES_ROUTES: Routes = [
-  {path: '', redirectTo: '', pathMatch: 'full'},
-  {path: '', component: MoviesComponent},
-  {path: 'new', component: MovieFormComponent},
-  {path: 'edit/:id', component: MovieFormComponent},
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: MoviesComponent },
+  {
+    path: 'new',
+    component: MovieFormComponent,
+    resolve: { movie: movieResolver },
+  },
+  {
+    path: 'edit/:id',
+    component: MovieFormComponent,
+    resolve: { movie: movieResolver },
+  },
 ];
